de5e8b40019e7d80160d2c5be80e27b001fb4a25
Solod Liudmyla <solodludmila@gmail.com>
2021-08-24 14:37:01 +0300

    5 lesson. CSS, SCSS loaders

--- webpack-course/webpack.config.js
+++ webpack-course/webpack.config.js
@@ -18,7 +18,18 @@ module.exports = {
                         presets: ['@babel/preset-env']
                     }
                 }
-            }
+            },
+            {
+                test: /\.scss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
         ]
     },
     plugins: [
